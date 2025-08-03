4dbf373314146bca8114bc0d6d0e8673bcb76b1a
Onlyakr <onlyakr2477@gmail.com>
2025-08-03 15:08:46 +0700

    fix: login bug 3

--- lib/users.ts
+++ lib/users.ts
@@ -2,25 +2,19 @@ import { loginFormSchema } from "@/schemas/authSchema";
 import { z } from "zod";
 
 export const loginUser = async (values: z.infer<typeof loginFormSchema>) => {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/api/users/login`,
-    {
-      method: "POST",
-      body: JSON.stringify(values),
-    },
-  );
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/user/login`, {
+    method: "POST",
+    body: JSON.stringify(values),
+  });
 
   const data = await res.json();
   return data;
 };
 
 export const logOutUser = async () => {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/api/users/logout`,
-    {
-      method: "POST",
-    },
-  );
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/user/logout`, {
+    method: "POST",
+  });
   const data = await res.json();
   return data;
 };
