25aea64946b808507468238279a17883293cc9b3
Onlyakr <onlyakr2477@gmail.com>
2025-08-04 19:52:19 +0700

    feat: protect route

---
+++ middleware.ts
@@ -0,0 +1,45 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { jwtDecode } from "jwt-decode";
+
+export function middleware(request: NextRequest) {
+  const token = request.cookies.get("jwt")?.value;
+  const path = request.nextUrl.pathname;
+
+  // Login routes
+  const isLoginRoute = path.startsWith("/login");
+
+  if (isLoginRoute) {
+    if (token) {
+      const user = jwtDecode(token);
+      if (user) {
+        return NextResponse.redirect(new URL("/products", request.url));
+      }
+      return NextResponse.next();
+    }
+  }
+
+  // Manager routes
+  const isManagerRoute =
+    path.startsWith("/stocks") || path.startsWith("/dashboard");
+
+  if (isManagerRoute) {
+    if (!token) {
+      return NextResponse.redirect(new URL("/login", request.url));
+    }
+
+    const user = jwtDecode(token);
+
+    if (!user || user.role !== "manager") {
+      return NextResponse.redirect(new URL("/products", request.url));
+    }
+
+    return NextResponse.next();
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/stocks/:path*", "/dashboard/:path*", "/login/:path*"],
+};
---
+++ utils/decodeJwt.ts
@@ -0,0 +1,13 @@
+import { jwtDecode } from "jwt-decode";
+
+export function decodeJwt(token: string) {
+  try {
+    const decoded = jwtDecode(token);
+    console.log(decoded);
+    if (decoded.exp && decoded.exp * 1000 < Date.now()) {
+      console.log("Token has expired.");
+    }
+  } catch (error) {
+    console.error("Invalid token:", error);
+  }
+}
