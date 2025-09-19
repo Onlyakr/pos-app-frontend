73f831c7df927164e2570af8a24290d2617adb43
Onlyakr <onlyakr2477@gmail.com>
2025-09-19 13:50:29 +0700

    feat: implemented dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,17 +1,29 @@
-import { Skeleton } from "@/components/ui/skeleton";
 import AppLayout from "@/components/layout/AppLayout";
+import BarChart from "@/components/dashboard/BarChart";
+import AreaChart from "@/components/dashboard/AreaChart";
 
 const DashboardPage = () => {
   return (
     <AppLayout>
-      <div className="grid size-full grid-cols-2 grid-rows-7 gap-2">
-        <Skeleton className="col-span-1 row-span-2" />
-        <Skeleton className="col-span-1 row-span-3" />
-        <Skeleton className="col-span-1 row-span-3" />
-        <Skeleton className="col-span-1 row-span-2" />
-        <Skeleton className="col-span-1 row-span-1" />
-        <Skeleton className="col-span-1 row-span-2" />
-        <Skeleton className="col-span-1 row-span-1" />
+      {/* <div className="flex h-full flex-1 items-center justify-center gap-2 overflow-auto">
+        <div className="flex h-full w-1/2 flex-col justify-between gap-2">
+          <div className="h-1/4 border border-red-500">chart 1</div>
+          <AreaChart className="h-3/4" />
+        </div>
+        <div className="flex h-full w-1/2 flex-col justify-between gap-2">
+          <BarChart className="h-3/4" />
+          <div className="h-1/4 border border-red-500">chart 2</div>
+        </div>
+      </div> */}
+      <div className="grid size-full grid-cols-1 gap-2 overflow-auto sm:grid-cols-2 sm:grid-rows-6">
+        <div className="border border-red-500 sm:row-span-1 lg:row-span-2">
+          chart 1
+        </div>
+        <AreaChart className="sm:row-span-5 lg:row-span-4" />
+        <BarChart className="sm:row-span-5 lg:row-span-4" />
+        <div className="border border-red-500 sm:row-span-1 lg:row-span-2">
+          chart 2
+        </div>
       </div>
     </AppLayout>
   );
--- components/layout/AppLayout.tsx
+++ components/layout/AppLayout.tsx
@@ -1,8 +1,5 @@
-import { Smile } from "lucide-react";
-import { Button } from "../ui/button";
 import AppHeader from "./header/AppHeader";
 import AppSidebar from "./sidebar/AppSidebar";
-import Link from "next/link";
 
 const AppLayout = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -12,17 +9,6 @@ const AppLayout = ({ children }: { children: React.ReactNode }) => {
         <AppHeader />
         <div className="bg-card border-border relative flex h-1/2 flex-1 items-start justify-center rounded-lg border p-2 shadow-md">
           {children}
-
-          {/* Funny button */}
-          {/* <Button
-            variant="destructive"
-            className="absolute right-2 bottom-2 size-11"
-            asChild
-          >
-            <Link href="/game">
-              <Smile />
-            </Link>
-          </Button> */}
         </div>
       </main>
     </>
